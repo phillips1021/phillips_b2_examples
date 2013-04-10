fd7f57eec40ba84b19959d1d03a009796b689dc8
Bruce Phillips <phillips1021@hotmail.com>
2013-04-10 11:29:44 -0500

    added more logging

--- B2_Servlet_Example_Part_Two/src/edu/ku/it/si/b2example/servlet/ThrowException.java
+++ B2_Servlet_Example_Part_Two/src/edu/ku/it/si/b2example/servlet/ThrowException.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 /**
  * Controller for demonstrating result of an 
  * unhandled exception.
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ThrowException extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
+	private static final Logger LOGGER = Logger.getLogger(ThrowException.class.getName() );
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -29,9 +31,10 @@ public class ThrowException extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		throw new ServletException("Unable to process your request.");
+		LOGGER.error("About to throw exception...");
 		
-		
+		throw new NullPointerException("Oh no! A null pointer excepton was thrown.") ;
+
 	}
 
 }
